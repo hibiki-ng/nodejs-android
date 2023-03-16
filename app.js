a075883ce169f6a6ba70328ea1e013ce8ab58f52
Hibiki <zerdiix.zanishiku@gmail.com>
2023-03-17 00:01:18 +0100

    rebuild app with test-node

--- app.js
+++ app.js
@@ -3,22 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
 
 var app = express();
-
-const uri = process.env.DB_URL;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Connected to MongoDB Atlas cluster");
-  }
-});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,6 +22,10 @@ app.use(function(req, res, next) {
   });
 
 app.use('/', indexRouter);
-app.use('/quotes', quotesRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
